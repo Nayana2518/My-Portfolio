d72d9a78c2cb688e330f3cdeca769b096faf897b
nayana-2518 <nayana@aliceblueindia.com>
2026-01-09 17:07:02 +0530

    implement navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
 import { AboutComponent } from './features/about/about.component';
 import { ProjectsComponent } from './features/projects/projects.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { MainComponent } from './shared/layout/main/main.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    {path: '', component: MainComponent },
     { path: 'about', component: AboutComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'contact', component: ContactComponent },

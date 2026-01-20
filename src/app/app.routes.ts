9ea39fe405946b349f1058efa1faaf379a7d9c80
nayana-2518 <nayana@aliceblueindia.com>
2026-01-20 17:37:29 +0530

    main body header part

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { AboutComponent } from './features/about/about.component';
 import { ProjectsComponent } from './features/projects/projects.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { MainComponent } from './shared/layout/main/main.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: MainComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'projects', component: ProjectsComponent },
-    { path: 'contact', component: ContactComponent },
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            { path: '', component: HomeComponent },
+            { path: 'about', component: AboutComponent },
+            { path: 'projects', component: ProjectsComponent },
+            { path: 'contact', component: ContactComponent }
+        ]
+    },
     { path: '**', redirectTo: '' }
 ];
--- src/app/shared/layout/main/main.component.ts
+++ src/app/shared/layout/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, SidebarComponent, RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
